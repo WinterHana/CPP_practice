bf165c42c2a95f040c8e3ad00efd3474cd3908df
Winter_Hana <haha920@naver.com>
2022-05-11 17:44:28 +0900

    220511 중간고사 코드 테스트

--- 220510_Test/Project1/Test.cpp
+++ 220510_Test/Project1/Test.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	string str;
+	getline(cin, str);
+	int num = 1;
+	int i = 0;
+	while (true)
+	{
+		if (i == str.length() - 1)
+		{
+			break;
+		}
+
+		cout << num << ':';
+
+		do
+		{
+			if (str[i] == ';')
+			{
+				i++;
+				continue;
+			}
+			cout << str[i];
+			i++;
+
+		} while (str[i] != ';');
+
+		num++;
+
+		cout << endl;
+	}
 
 	return 0;
 }
